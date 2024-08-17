34573777ef41df7504b9bac6103b20d0f84cceb1
VSytin <git.user.faloself@gmail.com>
2024-08-17 19:52:08 +0300

    Блок 3. Курсовой проект ч2

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Введите текст и нажмите <Enter> ");
-        String text = new Scanner(System.in) .nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.print("Введите первое число: ");
+        int firstNumber = scanner.nextInt();
+
+        System.out.print("Введите второе число: ");
+        int secondNumber = scanner.nextInt();
+
+        int sum = firstNumber + secondNumber;
+        int difference = firstNumber - secondNumber;
+        int product = firstNumber * secondNumber;
+
+              double quotient = (double) firstNumber / secondNumber;
+
+        // Выводим результаты
+        System.out.println("Сумма: " + sum);
+        System.out.println("Разность: " + difference);
+        System.out.println("Произведение: " + product);
+        System.out.println("Частное: " + quotient);
+
     }
 }
