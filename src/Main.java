a4f0ee44fd73df3ca74644750cbe5755a8fb992e
VSytin <git.user.faloself@gmail.com>
2024-08-19 21:44:03 +0300

    Блок 5. Курсовой проект ч1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,27 @@
+import java.io.File;
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+           public class Main {
+            public static void main(String[] args) {
+                Scanner scanner = new Scanner(System.in);
+                int validFileCount = 0; // Счётчик корректных файлов
 
-        System.out.println("Введите текст и нажмите <Enter> ");
-        String text = new Scanner(System.in) .nextLine();
-        System.out.println("Длина текста: " + text.length());
-    }
-}
+                while(true) {
+                    System.out.println("Введите путь к файлу");
+                    String path = scanner.nextLine();
+
+                    File file = new File(path);
+                    boolean fileExists = file.exists(); // Проверка существования файла
+                    boolean isDirectory = file.isDirectory(); // Проверка, является ли это папкой
+
+                    // Проверка входных данных
+                    if (!fileExists || isDirectory) {
+                        System.out.println("Указанный путь не существует или это папка. Попробуйте снова.");
+                        continue; // Продолжение цикла
+                    }
+                    // Если файл существует и это файл
+                    validFileCount++;
+                    System.out.println("Путь указан верно. Это файл номер " + validFileCount);
+                }
+                            }
+        }
